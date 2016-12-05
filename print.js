528a82059c4236629216194fb103ca2d7e68e361
Orhan Hirsch <orhanhenrik@gmail.com>
2016-12-05 22:11:07 +0100

    Added print queue

--- print.js
+++ print.js
@@ -1,76 +1,75 @@
 var SP = require('serialport');
-
 var port = new SP('/dev/usb/lp0', {autoOpen:false})
-
 var qr = require('qr-image');
-var matrix = qr.matrix('Hei Larsen. Hvordan går det, kjære?');
 
-port.open(function(err){
-	if(err) return console.error(err);
-//	console.log('open!');
-	port.write(new Buffer([0x1b, 0x40, 0x1b, 0x64, 1]), function(){
-//		console.log('initialized');
-		// printText(port);	
-		port.drain(function(){
-			printBitmap(port);
-		});
-	});
-});
-
-function printText(port) {
-	var buffer = new Buffer("hello world\n");
-	buffer = Buffer.concat([buffer, new Buffer([0x0a])])
-	console.log('writing data');
-	port.write(buffer, function(err){
-		console.log('wrote data', err);
-		port.close();
-	});
+function getPort(cb) {
+  if (port.isOpen) {
+    cb(port);
+  } else {
+    port.open(function(err){
+      if(err) return console.error(err);
+      cb(port);
+    });
+  }
+}
+function closePort(cb) {
+  port.drain(function(){
+    setTimeout(function(){
+      port.close(cb);
+    }, 500);
+  });
 }
 
-function printBitmap(port) {
-	// 			  GS    v     0,    m xL xH yL yH d1..dk
-//	console.log(matrix);
+var printQueue = [
+  printMatrixJob(qr.matrix("Hei Larsen")),
+  printMatrixJob(qr.matrix("Hei Orhan")),
+  printMatrixJob(qr.matrix("Hei Sylliaas")),
+];
 
+var printing = false;
 
-	width = matrix.length;
-	height = matrix.length; // +10 -> padding at bottom
+function processPrintQueue() {
+  if(!printing) {
+    printing = true;
+    getPort(function(port) {
+      job = printQueue.shift();
+      job(port, function(){
+        printing = false;
+        processPrintQueue();
+      })
+    });
+  }
+}
+processPrintQueue();
 
-	m = 0x30;
-	xL = width
-	xH = 0;
-	yL = height*8 % 256;
-	yH = Math.floor(height*8 / 256);
-	
-	var pixels = [];
-	for (var i=0; i<xL*(yL+yH*256); i++){
-		y_index = Math.floor(i / xL / 8);
-		x_index = (i - y_index*8*xL) % xL;
+function printMatrixJob(matrix) {
+  return function(port, cb) {
+    printMatrix(port, matrix, cb);
+  }
+}
 
-		if (y_index < matrix.length) {
-			pixels[i] = matrix[y_index][x_index] * 255;
-		} else {
-			// padding pixels
-			pixels[i] = 255;
-		}
-	}
-//	console.log(xL, xH, yL, yH);
-//	console.log(pixels.length);
-	// port.close();return;
+function printMatrix(port, matrix, cb) {
+  width = matrix.length;
+  height = matrix.length;
+
+  m = 0x30;
+  xL = width
+  xH = 0;
+  yL = height*8 % 256;
+  yH = Math.floor(height*8 / 256);
+
+  var pixels = [];
+  for (var i=0; i<xL*(yL+yH*256); i++){
+    y_index = Math.floor(i / xL / 8);
+    x_index = (i - y_index*8*xL) % xL;
+    pixels[i] = matrix[y_index][x_index] * 255;
+  }
 
 	var buffer = Buffer.concat([
-		new Buffer([0x1d, 0x76, 0x30, 48, xL, xH, yL, yH]), 
+		new Buffer([0x1d, 0x76, 0x30, 48, xL, xH, yL, yH]),
 		new Buffer(pixels),
 		new Buffer([0x1b, 0x64, 2])
 	]);
-	// var buffer = new Buffer([0x1b, 0x64, 10])
-	
-//	console.log('printing buffer', buffer.length);
-	console.log(buffer.toString());
-	port.write(buffer, function(err) {
-		console.log('done 1');
-		port.drain(function(){
-			console.log('wrote data', err);
-			setTimeout(function(){port.close();}, 300);
-		});
-	});
+
+	port.write(buffer, cb);
 }
