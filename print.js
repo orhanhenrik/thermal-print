b6b7c44b8ab7d4629e83e341f02504e3a50205bb
orhan@pizero <orhanhenrik@gmail.com>
2016-12-06 00:52:05 +0000

    fixed everything

--- print.js
+++ print.js
@@ -1,5 +1,5 @@
 var SP = require('serialport');
-var port = new SP('/dev/usb/lp0', {autoOpen:false})
+var port = new SP('/dev/usb/lp0', {autoOpen:false, baudRate: 57600})
 var qr = require('qr-image');
 
 function getPort(cb) {
@@ -21,7 +21,7 @@ function closePort(cb) {
 }
 
 var printQueue = [
-  printMatrixJob(qr.matrix("Hei Larsen")),
+ // printMatrixJob(qr.matrix("Hei Larsen")),
   printMatrixJob(qr.matrix("Hei Orhan")),
   printMatrixJob(qr.matrix("Hei Sylliaas")),
 ];
@@ -53,42 +53,69 @@ function printMatrixJob(matrix) {
 }
 
 function printMatrix(port, matrix, cb) {
-  width = 384;// matrix.length;
-  height = 384;// matrix.length;
+  width = matrix.length * 8 * 2 // * Math.floor(48/matrix.length) //384;
+  height = matrix.length * 8 * 2 // * Math.floor(48/matrix.length) //384;
 
-  m = 0x30;
-  xL = int(width / 8);
-  xH = 0;
+height -= 1; // ?!?!?!? If height is not decreased, it does not print the whole buffer
+
+  m = 48;
+  xL = Math.floor(width / 8) % 256;
+  xH = Math.floor(Math.floor(width / 8) / 256);
   yL = height % 256;
   yH = Math.floor(height / 256);
 
-  var pixels = [];
-  for(var y = 0; y < height; y++) {
-    var byte = 0;
-    for(var x = 0; x < width; x+=8) {
-      for(var k=0; k < 8; k++) {
-        value = 1;
-        byte |= value << (7-k)
-      }
-    }
-    pixels.push(byte);
-  }
+  console.log(m, xL, xH, yL, yH);
 
-  /*
-  for (var i=0; i<xL*(yL+yH*256); i++){
-    y_index = Math.floor(i / xL / 8);
-    x_index = (i - y_index*8*xL) % xL;
-    pixels[i] = matrix[y_index][x_index] * 255;
+  var pixels = [];
+
+  console.log('generating bitmap');
+
+  for(var y = 0; y < height; y++) {
+    for(var x = 0; x < width; x+=8) {
+      var byte = 0;
+      for(var k=0; k < 8; k++) {
+        y_index = Math.floor(y * matrix.length / height);
+        x_index = Math.floor((x+k) * matrix.length / width);
+        value = matrix[y_index][x_index];
+        byte |= value << (7-k);
+      }
+      pixels.push(byte);
+    }
   }
-  */
+  console.log('generated');
+  
+  console.log((xL+xH*256)*(yL+yH*256), pixels.length);
+
 
   var buffer = Buffer.concat([
-    new Buffer([0x1d, 0x76, 0x30, 48, xL, xH, yL, yH]),
+    // new Buffer([0x1b, 0x40]), // ESC @
+    new Buffer([0x1d, 0x76, 0x30, m, xL, xH, yL, yH]),
     new Buffer(pixels),
     new Buffer([0x1b, 0x64, 2])
   ]);
 
-  port.write(buffer, function(){
+  writeAndDrain(port, buffer, function(){
+    console.log('wrote data');
+    w(0);
+    setTimeout(cb, 0);
+  });
+}
+
+function writeAndDrain(port, data, cb){
+  port.write(data, function(){
     port.drain(cb);
   });
 }
+
+function w(i){return;
+  console.log(i);
+  writeAndDrain(port, i+"", function(){
+    setTimeout(function(){w(i+1)}, 100);  
+  })
+}
+
+port.on('close', function(){console.log('closed');});
+
+port.on('data', function(d){console.log('data', d);});
+
+port.on('error', function(e){console.log('err', e);});
